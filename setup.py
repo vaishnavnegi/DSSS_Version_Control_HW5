25839c6b1fa291f2bb80a614bf135d5f09765232
vaishnavnegi <vaishnavnegi207@gmail.com>
2022-11-29 15:44:03 +0100

    changes to make repo pip installable

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from distutils.core import setup
+pip install setuptools
+from setuptools import setup
 from setuptools import find_packages
 
-setup(name='snowflake',
-      version='0.1',
-      author='Vaishnav Negi',
-      author_email='vaishnavnegi207@gmail.com',
-      packages=find_packages(),
-      install_requires=["numpy","turtle"],
-     )
+setup(
+    name='snowflake',
+    version='0.1.0',    
+    description='DSSS HW5 Version Control',
+    author='Vaishnav Negi',
+    author_email='vaishnavnegi207@gmail.com',
+    packages = find_packages(),
+    install_requires=['mpi4py>=2.0',"numpy","turtle"],
+    )
