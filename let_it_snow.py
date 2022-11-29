be51e5d9fa472c9e65f08600f840fbaa6c5f70aa
vaishnavnegi <vaishnavnegi207@gmail.com>
2022-11-29 14:24:43 +0100

    Edited let_it_snow.py to show random colored snowflakes.

--- let_it_snow.py
+++ let_it_snow.py
@@ -1,5 +1,6 @@
 import turtle
 import numpy as np
+from random import randint
 
 
 def main(speed=0, bg_color="grey"):
@@ -14,7 +15,7 @@ def main(speed=0, bg_color="grey"):
   
  
     """TODO: define different colors here"""
-
+    turtle.colormode(255)
 
     for _ in range(10):
         # define some params
@@ -22,6 +23,7 @@ def main(speed=0, bg_color="grey"):
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
+        myTurtle.pencolor(randint(0,255),randint(0,255),randint(0,255))
 
         # Go to the start position of the snowflake
         myTurtle.penup()
